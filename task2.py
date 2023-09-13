53a973ab4000bd4c189590efb9826787a5c2b7d7
Kosten-73 <49977127+Kosten-73@users.noreply.github.com>
2023-09-13 09:27:03 +0300

    доделал 2 задание

--- task2.py
+++ task2.py
@@ -1,18 +1,34 @@
 from PIL import Image
 from matplotlib import pyplot as plt
 import numpy as np
-
+plt.figure(num='График 2 задания')
 def bloki(width, height, im):
-    mas = []
+    r_m = [0] * 256
+    g_m = [0] * 256
+    b_m = [0] * 256
     for i in range(width):
         for j in range(height):
             # получаем яркость текущего пикселя
             r, g, b = im.getpixel((i, j))
+            loc = 0
             for now in r, g, b:
-                mas.append(now)
+                if loc == 0:
+                    r_m[now] += 1
+                if loc == 1:
+                    g_m[now] += 1
+                if loc == 2:
+                    b_m[now] += 1
+                loc += 1
+
+    plt.plot(r_m, color='red')
+    plt.plot(g_m, color='green')
+    plt.plot(b_m, color='blue')
+    plt.xlabel("оттенок")
+    plt.ylabel("количество")
     # plt.legend()
-    # plt.show()
-    return mas
+    plt.show()
+
+
 
 
 im = Image.open("pic.jpg")
@@ -22,4 +38,4 @@ im = im.convert("RGB")
 # получаем размеры картинок
 width, height = im.size
 
-print(bloki(width, height, im))
+bloki(width, height, im)
